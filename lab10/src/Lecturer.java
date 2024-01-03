dddafce9194ef586f71100086d89c9126f5f68f8
FarJoke <68071634+FarJoke@users.noreply.github.com>
2024-01-03 12:19:42 +0300

    Add files via upload

---
+++ lab10/src/Lecturer.java
@@ -0,0 +1,27 @@
+
+public class Lecturer extends ResearchAssociate {
+
+	/**
+	 * 
+	 * @param classNumber
+	 */
+
+	public Lecturer(int ssNo, String name, Date birthday, String email, int salary) {
+		super(ssNo, name, birthday, email, salary);
+	}
+
+	public void conductClass(int classNumber) {
+		// TODO - implement Lecturer.conductClass
+
+		String selectedMaterial = cource.material.substring(classNumber * 25, (classNumber + 1) * 25);
+
+		// read selected material for students
+
+		throw new UnsupportedOperationException();
+	}
+
+	public String toString() {
+		return this.ssNo + " " + this.name + " " + this.birthday + " " + this.email + " " + this.salary + " " + this.balance;
+	}
+
+}
---
+++ lab10/src/Participation.java
@@ -0,0 +1,14 @@
+
+public class Participation {
+
+	public double hours = 0;
+
+	public Participation() {
+		
+	}
+
+	public String toString() {
+		return this.hours;
+	}
+
+}
